84b4905f2a5d835262ee3875b04bd1b3d5379443
Radosław Rudnik <rudnikrado@gmail.com>
2023-12-06 16:44:59 +0100

    Number ten

---
+++ Projects/Lekcje JnsP/10-petla-while-for.py
@@ -0,0 +1,15 @@
+number = 1
+
+# while number < 6:
+#     print(number)
+#     number += 1
+
+# for number in range(1, 6, 2):
+#     if number == 5:
+#         break
+#     print(number)
+
+for number in range(1, 10):
+    if number == 5:
+        continue
+    print(number)
